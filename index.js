7a62dcd1373a9eb5ebba5f6ae0b9be9e4a5b15fc
Eveline Guerrero <evelineguerrero23@gmail.com>
2021-12-20 20:55:26 -0500

    commit tree c2414f1c11be7e258a27a2bfcde9124ce632c345

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Your code goes here
+// document.addEventListener("DOMContentLoaded", function (){
+//    const text =  document.querySelector('#text');
+   
+//    text.textContent = 'This is really cool!';
+//    document.querySelector('p').appendChild(text);
+    
+
+// })
+
+
+
+// document.addEventListener('DOMContentLoaded', function(){
+//     const text = document.querySelector('#text')
+
+//     text.innerHTML = "This is really cool!";
+//     document.querySelector('p').appendChild(text)
+// })
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const text = document.querySelector('#text');
+    text.textContent = "This is really cool!";
+    document.querySelector('p').appendChild(text)
+    
+})
